d36a8a595d3aab16ab84345b2aef24c27d7fc1da
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-07-28 14:30:42 -0400

    Updated video and screenshare hostfiles to properly connect to distribution servers and send data

--- server/stream_screenshare_host.py
+++ server/stream_screenshare_host.py
@@ -1,5 +1,4 @@
 import socket
-import threading
 import pyautogui
 import base64
 from io import BytesIO
@@ -7,11 +6,11 @@ import time
 
 DISTRIBUTION_HOST = "localhost"
 DISTRIBUTION_PORT = 12348
-
-HOST = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST.connect((DISTRIBUTION_HOST, DISTRIBUTION_PORT))
+FRAME_RATE = 10  # Frames per second
 
 def send_screen(host):
+    frame_interval = 1.0 / FRAME_RATE  # Time interval between frames
+
     while True:
         try:
             screenshot = pyautogui.screenshot()
@@ -23,15 +22,21 @@ def send_screen(host):
 
             frame_encode = base64.b64encode(screenshot_bytes).decode("utf-8")
             host.sendall((frame_encode + "<END>").encode("utf-8"))
-            time.sleep(0.1)  # Add a small delay to control the frame rate
+
+            time.sleep(frame_interval)  # Add a delay to control the frame rate
         except Exception as e:
             print(f"Error sending screen info: {e}")
             host.close()
             break
 
 def screen_main():
-    screen_thread = threading.Thread(target=send_screen, args=(HOST,))
-    screen_thread.start()
+    try:
+        host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host_socket.connect((DISTRIBUTION_HOST, DISTRIBUTION_PORT))
+        print(f"Connected to distribution server at {DISTRIBUTION_HOST}:{DISTRIBUTION_PORT}")
+        send_screen(host_socket)
+    except Exception as e:
+        print(f"Error connecting to distribution server: {e}")
 
 if __name__ == "__main__":
     screen_main()
